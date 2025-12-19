75be340050228749dbfa15a41152aa9685d30721
AjayKumarN-2255 <kumar.ajay@midnay.com>
2025-12-19 11:50:09 +0530

    completed listing single item based on slug

--- src/app/events/[slug]/page.jsx
+++ src/app/events/[slug]/page.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { Suspense } from 'react';
+import EventDetails from '@/components/EventDetails';
 
-function page() {
+const EventDetailsPage = async ({ params }) => {
+  // Extract slug from params
+  const res = await params;
   return (
-    <div>single event page</div>
-  )
-}
+    <main>
+      <Suspense fallback={<div>Loading...</div>}>
+        <EventDetails slug={res.slug} />
+      </Suspense>
+    </main>
+  );
+};
 
-export default page
+export default EventDetailsPage;
